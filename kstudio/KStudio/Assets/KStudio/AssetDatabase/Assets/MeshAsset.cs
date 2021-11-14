2a143cf5307674b60f192ac5b8a0f79729371b4f
gebo-10 <937319738@qq.com>
2021-11-14 15:46:31 +0800

    直接将序列化文件输出到工程目录

---
+++ kstudio/KStudio/Assets/KStudio/AssetDatabase/Assets/MeshAsset.cs
@@ -0,0 +1,74 @@
+﻿using kserialize;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KStudio
+{
+    class MeshAsset: Asset
+    {
+        public kserialize.ShaderT shader=new kserialize.ShaderT();
+        public enum State
+        {
+            Nil,
+            Decl,
+            Vert,
+            Frag,
+        }
+        State state;
+        public MeshAsset(string path)
+        {
+            state = State.Nil;
+            var lines= File.ReadAllLines(path);
+            foreach(var line in lines)
+            {
+                if (line.Contains("#decl"))
+                {
+                    state = State.Decl;
+                    continue;
+                }else if(line.Contains("#frag"))
+                {
+                    state = State.Frag;
+                    continue;
+                }
+                else if (line.Contains("#vert"))
+                {
+                    state = State.Vert;
+                    continue;
+                }
+
+                switch (state)
+                {
+                    case State.Nil:
+
+                        break;
+                    case State.Decl:
+
+                        break;
+                    case State.Vert:
+                        shader.Vert += line;
+                        shader.Vert += "\n";
+                        break;
+                    case State.Frag:
+                        shader.Frag += line;
+                        shader.Frag += "\n";
+                        break;
+                }
+            }
+        }
+        public byte[] Serialize()
+        {
+            return shader.SerializeToBinary();
+        }
+
+        public override void InitCommon(AssetType type, uint id)
+        {
+            shader.AssetCommon = new kserialize.AssetCommonT();
+            shader.AssetCommon.AssetType = type;
+            shader.AssetCommon.Id = id;
+        }
+    }
+}
